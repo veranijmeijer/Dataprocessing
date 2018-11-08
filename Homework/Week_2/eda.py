a7362716d975e42f1f95f6e0e6c4ad81b289f06a
veranijmeijer <43072285+veranijmeijer@users.noreply.github.com>
2018-11-08 13:09:37 +0100

    clean outputfile added

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,1 +1,19 @@
+import csv
 import pandas
+
+clean_rows = []
+with open("input.csv", "r") as csvfile:
+    for row in csvfile:
+        row = row.rstrip().split(",")
+        empty_column = False
+        for column in row:
+            if not column:
+                empty_column = True
+
+        if not empty_column:
+            clean_rows.append(row)
+
+with open("clean_input.csv", "w") as cleanfile:
+    writer = csv.writer(cleanfile)
+    for clean_row in clean_rows:
+        writer.writerow(clean_row)
