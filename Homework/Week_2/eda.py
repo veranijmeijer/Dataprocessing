ccd7732776477361d0e1affca15b1205c0153ea9
veranijmeijer <43072285+veranijmeijer@users.noreply.github.com>
2018-11-09 15:56:59 +0100

    table cleaned

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -6,8 +6,11 @@ clean_rows = []
 inputfile = open("input.csv", "r")
 outputfile = open("output.csv", "w")
 
+inputfile.readline()
+
 for row in inputfile:
     if row != "\n":
+        print(row)
         row_csv = [row.rstrip()]
         empty_column = False
         for column in row_csv:
@@ -19,7 +22,9 @@ for row in inputfile:
 
 
 writer = csv.writer(outputfile)
+writer.writerow(['Country', 'Region', 'Population', 'Area (sq. mi.)', 'Pop. Density (per sq. mi.)', 'Coastline (coast/area ratio)', 'Net migration', 'Infant mortality (per 1000 births)', 'GDP ($ per capita) dollars', 'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', "Other (%)", "Climate", "Birthrate", "Deathrate", "Agriculture", "Industry", "Service"])
 for clean_row in clean_rows:
+    # print(clean_row)
     writer.writerow(clean_row)
 
 inputfile.close()
