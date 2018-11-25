f071247b991739a9c0567d00c0ac7f71db367b47
veranijmeijer <43072285+veranijmeijer@users.noreply.github.com>
2018-11-25 23:59:18 +0100

    index

---
+++ Homework/Week_4/convertCSV2JSON.py
@@ -0,0 +1,36 @@
+# Name: Vera Nijmeijer
+# Student ID: 10753567
+
+import csv
+import json
+
+def create_json(infile, outfile):
+    # function loads data from csv (or txt) file to json file
+
+    with open(infile) as input:
+        # saves first row as fieldnames
+        names = input.readline().strip().split(',')
+        fieldnames = []
+        dict_dates = []
+
+        # removes "" from fieldnames
+        for name in names:
+            fieldnames.append(name[1:-1])
+
+        # extracts information from csv file into dictionary
+        dates = csv.DictReader(input, fieldnames=fieldnames)
+
+        for date in dates:
+            date_info = {}
+
+            # makes dictionary with the information about this date
+            for fieldname in fieldnames:
+                date_info[fieldname] = date[fieldname].strip()
+
+            dict_dates.append(date_info)
+
+        # create JSON file
+        with open(outfile, "w") as output:
+            json.dump(dict_dates, output, indent=4)
+
+create_json("data.csv", "data.json")
